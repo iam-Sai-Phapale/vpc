74f98114f6d1156674fb804fc14c4138815f5825
sai <saiphpale7272@gamil.com>
2024-02-27 17:27:23 +0530

    new

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "web" {
   instance_type = var.size
   subnet_id = aws_subnet.demo_subnet.id
   vpc_security_group_ids = [ aws_security_group.demo-vpc-sg.id]
+  availability_zone = "ap-south-1a"
 
   tags = {
     Name = "vpc-example"
@@ -24,6 +25,8 @@ resource "aws_subnet" "demo_subnet" {
   vpc_id     = aws_vpc.demo_vpc.id
   cidr_block = var.subnet-1-cidr
   map_public_ip_on_launch = true
+  availability_zone = "ap-south-1a"
+
 
   tags = {
     Name = "demo subnet"
