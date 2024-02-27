9e78ee12add41ac4c532b94aed55ba949d5033ab
sai <saiphpale7272@gamil.com>
2024-02-27 17:16:04 +0530

    new

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "ap-south-1"
+    region = var.region
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-03f4878755434977f"
-  instance_type = "t2.micro"
+  ami           = var.os_name
+  instance_type = var.size
   subnet_id = aws_subnet.demo_subnet.id
   vpc_security_group_ids = [ aws_security_group.demo-vpc-sg.id]
 
@@ -16,13 +16,13 @@ resource "aws_instance" "web" {
 
 //cerate vpc
 resource "aws_vpc" "demo_vpc" {
-  cidr_block = "10.10.0.0/16"
+  cidr_block = var.cidr_block_id
 }
 
 //create subnet
 resource "aws_subnet" "demo_subnet" {
   vpc_id     = aws_vpc.demo_vpc.id
-  cidr_block = "10.10.1.0/24"
+  cidr_block = var.subnet-1-cidr
   map_public_ip_on_launch = true
 
   tags = {
@@ -83,6 +83,7 @@ resource "aws_security_group" "demo-vpc-sg" {
     }
 
 }
+
 
 
 
---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "region" {
+    default = "ap-south-1"
+}
+
+variable "os_name" {
+    default = "ami-03f4878755434977f"
+}
+
+variable "size" {
+    default = "t2.micro"
+}
+
+variable "cidr_block_id" {
+    default = "10.10.0.0/16" 
+}
+
+variable "subnet-1-cidr" {
+    default = "10.10.1.0/16"
+}
