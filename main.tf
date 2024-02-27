432afef34f62c34d4f40b7351266c37f17f94df0
sai <saiphpale7272@gamil.com>
2024-02-27 16:51:30 +0530

    new

--- main.tf
+++ main.tf
@@ -23,6 +23,7 @@ resource "aws_vpc" "demo_vpc" {
 resource "aws_subnet" "demo_subnet" {
   vpc_id     = aws_vpc.demo_vpc.id
   cidr_block = "10.10.1.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "demo subnet"
