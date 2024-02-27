2ef813751586504d800540ef0114e1c93918f5cb
sai <saiphpale7272@gamil.com>
2024-02-27 16:39:59 +0530

    new

---
+++ main.tf
@@ -0,0 +1,89 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-03f4878755434977f"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.demo_subnet.id
+  vpc_security_group_ids = [ aws_security_group.demo-vpc-sg.id]
+
+  tags = {
+    Name = "vpc-example"
+  }
+}
+
+
+//cerate vpc
+resource "aws_vpc" "demo_vpc" {
+  cidr_block = "10.10.0.0/16"
+}
+
+//create subnet
+resource "aws_subnet" "demo_subnet" {
+  vpc_id     = aws_vpc.demo_vpc.id
+  cidr_block = "10.10.1.0/24"
+
+  tags = {
+    Name = "demo subnet"
+  }
+}
+
+resource "aws_internet_gateway" "demo_igw" {
+  vpc_id = aws_vpc.demo_vpc.id
+
+  tags = {
+    Name = "demo igw"
+  }
+}
+
+resource "aws_route_table" "demo-rt" {
+  vpc_id = aws_vpc.demo_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.demo_igw.id
+  }
+
+  tags = {
+    Name = "demo-rt"
+  }
+}
+
+//asscoaite subnet 
+resource "aws_route_table_association" "demo-rt_assocaition" {
+  subnet_id      = aws_subnet.demo_subnet.id
+  route_table_id = aws_route_table.demo-rt.id
+}
+
+resource "aws_security_group" "demo-vpc-sg" {
+    name = "demo-vpc-sg"
+    vpc_id = aws_vpc.demo_vpc.id
+
+    ingress {
+        description = "TLS from vpc"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_block = ["0.0.0.0/0"]
+        ipv6_cidr_block = ["::/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "tcp"
+        cidr_block = ["0.0.0.0/0"]
+        ipv6_cidr_block = ["::/0"]
+    }
+
+    tags = {
+      Name = "demo-vpc-sg"
+    }
+
+}
+
+
+
+
+
